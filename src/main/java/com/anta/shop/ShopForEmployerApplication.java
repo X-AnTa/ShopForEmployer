bc9bf4a8188a33fcbb45d421b1d876eeb0d7f000
X-AnTa <mrspbusdriver@gmail.com>
2022-01-24 22:44:21 +0600

    Spring Boot app

---
+++ src/main/java/com/anta/shop/ShopForEmployerApplication.java
@@ -0,0 +1,13 @@
+package com.anta.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopForEmployerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopForEmployerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anta/shop/ShopForEmployerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anta.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopForEmployerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
