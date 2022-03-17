740ee16cac2496afbfe27fa6203cd744ffd5a372
X-AnTa <mrspbusdriver@gmail.com>
2022-03-17 19:22:19 +0600

    .

--- src/main/java/com/anta/shop/repository/ProductRepository.java
+++ src/main/java/com/anta/shop/repository/ProductRepository.java
@@ -2,13 +2,12 @@ package com.anta.shop.repository;
 
 import com.anta.shop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     List<Product> findAllByName(String name);
 
