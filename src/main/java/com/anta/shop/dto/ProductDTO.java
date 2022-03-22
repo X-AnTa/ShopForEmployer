9552951c9ac3cf81369d41a5af47225adec1b556
X-AnTa <mrspbusdriver@gmail.com>
2022-03-22 11:40:56 +0600

    add Lombok

--- src/main/java/com/anta/shop/dto/ProductDTO.java
+++ src/main/java/com/anta/shop/dto/ProductDTO.java
@@ -1,14 +1,14 @@
 package com.anta.shop.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Date;
 import java.util.Set;
 
+@Data
 public class ProductDTO {
 
     private int id;
@@ -21,68 +21,4 @@ public class ProductDTO {
     private Date dateOfModification;
     private Set<String> currencies;
     private Set<String> descriptions;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = new BigDecimal(String.valueOf(price)).setScale(2, RoundingMode.DOWN);
-    }
-
-    public Date getDateOfCreation() {
-        return dateOfCreation;
-    }
-
-    public void setDateOfCreation(Date dateOfCreation) {
-        this.dateOfCreation = dateOfCreation;
-    }
-
-    public Date getDateOfModification() {
-        return dateOfModification;
-    }
-
-    public void setDateOfModification(Date dateOfModification) {
-        this.dateOfModification = dateOfModification;
-    }
-
-    public Set<String> getCurrencies() {
-        return currencies;
-    }
-
-    public void setCurrencies(Set<String> currencies) {
-        this.currencies = currencies;
-    }
-
-    public Set<String> getDescriptions() {
-        return descriptions;
-    }
-
-    public void setDescriptions(Set<String> descriptions) {
-        this.descriptions = descriptions;
-    }
 }
